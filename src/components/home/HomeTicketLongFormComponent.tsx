be7a2751cd4a279a45d90a102b2389087446ba5f
HungPham <hungpg@vietpointer.vn>
2023-03-14 16:49:34 +0700

    update home screen

---
+++ src/components/home/HomeTicketLongFormComponent.tsx
@@ -0,0 +1,195 @@
+import {HomeCountdownClockComponent} from '@components';
+import {BorderComponent, Icon, Image, Label, ScreenUtils} from '@shared';
+import {Color, Style} from '@styles';
+import React, {useCallback, useMemo} from 'react';
+import {TouchableOpacity, View, ViewProps} from 'react-native';
+
+export interface HomeTicketLongFormComponent extends ViewProps {
+  image?: string;
+  title?: string;
+  totalPrize?: string;
+  countdownTime?: string;
+
+  leftView?: () => JSX.Element;
+  rightView?: () => JSX.Element;
+  centerView?: () => JSX.Element;
+  type?: 'none' | 'max' | 'mega' | 'power' | 'max-pro' | 'keno' | 'group';
+}
+
+export const HomeTicketLongFormComponent = React.memo(
+  (props?: HomeTicketLongFormComponent) => {
+    const mainColor = useMemo(() => {
+      switch (props?.type) {
+        case 'max': {
+          return Color.purple;
+        }
+        case 'mega': {
+          return Color.red;
+        }
+        case 'power': {
+          return Color.yellow;
+        }
+        case 'max-pro': {
+          return Color.black;
+        }
+        case 'keno': {
+          return Color.orange;
+        }
+        default: {
+          return Color.green;
+        }
+      }
+    }, [props?.type]);
+
+    const renderImage = useCallback(() => {
+      return (
+        <View
+          style={[
+            Style.Space.Padding.Medium_12,
+            Style.Background.White,
+            {
+              height: ScreenUtils.getSizeByHorizontal(90),
+              width: ScreenUtils.getSizeByHorizontal(90),
+              borderTopLeftRadius: 10,
+              borderBottomLeftRadius: 10,
+            },
+          ]}>
+          <Image
+            source={{
+              uri: props?.image,
+            }}
+            style={[Style.Size.WidthMatchParent, Style.Size.HeightMatchParent]}
+            resizeMode="contain"
+          />
+          <BorderComponent
+            width={20}
+            height={20}
+            borderStyle={[{backgroundColor: Color.red, borderColor: Color.red}]}
+            style={[
+              Style.AbsolutePosition.LeftTop,
+              {
+                transform: [{rotate: '0deg'}],
+              },
+            ]}
+          />
+          <BorderComponent
+            width={20}
+            height={20}
+            borderStyle={[{backgroundColor: Color.red, borderColor: Color.red}]}
+            style={[
+              {
+                position: 'absolute',
+                left: 0,
+                bottom: 0,
+                transform: [{rotate: '270deg'}],
+              },
+            ]}
+          />
+        </View>
+      );
+    }, [props?.image]);
+
+    const renderDescription = useCallback(() => {
+      return (
+        <View>
+          <View
+            style={[Style.Size.FlexRow, Style.ContentFlexRow.CenterInVertical]}>
+            <Label.Widget style={[Style.Label.Regular.GrayContent_13]}>
+              {props?.type !== 'keno'
+                ? 'Kỳ QSMT 14/03/2023'
+                : 'Keno/Bao Keno/AI Keno'}
+            </Label.Widget>
+            <Label.Widget
+              style={[
+                Style.Label.Regular.RedContent_13,
+                {color: mainColor},
+                Style.Space.PaddingHorizontal.Small_8,
+                Style.Space.MarginLeft.medium_12,
+                Style.Border.generateBorderStyle({
+                  borderColor: mainColor,
+                  borderRadius: 9,
+                  borderWidth: 1,
+                }),
+              ]}>
+              {props?.type !== 'keno' ? 'T4, T6, CN' : 'Cả tuần'}
+            </Label.Widget>
+          </View>
+          <Label.Widget
+            style={[
+              Style.Label.Bold.GrayContentXL_16,
+              Style.Space.MarginTop.small_8,
+              {color: mainColor},
+            ]}>
+            {'56.487.021.150 đ'}
+          </Label.Widget>
+          <HomeCountdownClockComponent
+            targetTime={new Date('2023-03-15T18:00:00Z')}
+          />
+        </View>
+      );
+    }, [mainColor]);
+
+    const renderRightView = useCallback(() => {
+      return (
+        <Icon.Default
+          name="ic_arrow"
+          size={'small'}
+          color={mainColor}
+          style={[Style.Space.Padding.Zero, Style.Space.PaddingRight.Tiny_4]}
+        />
+      );
+    }, [mainColor]);
+
+    return (
+      <TouchableOpacity
+        style={[
+          Style.Size.MatchParentRow,
+          Style.Space.MarginTop.medium_12,
+          Style.ContentFlexRow.CenterInVertical,
+          Style.Space.MarginBottom.small_8,
+          props?.style,
+        ]}>
+        {props?.leftView ? props?.leftView() : renderImage()}
+        <View
+          style={[
+            Style.Size.MatchParentRow,
+            Style.ContentFlexRow.CenterInVertical,
+            Style.Background.White,
+            Style.Space.PaddingLeft.Small_8,
+            {
+              justifyContent: 'space-between',
+              height: ScreenUtils.getSizeByHorizontal(90),
+              marginLeft: 0.5,
+              borderTopRightRadius: 10,
+              borderBottomRightRadius: 10,
+            },
+          ]}>
+          {props?.centerView ? props?.centerView() : renderDescription()}
+          {props?.rightView ? props?.rightView() : renderRightView()}
+          <BorderComponent
+            width={20}
+            height={20}
+            borderStyle={[{backgroundColor: Color.red, borderColor: Color.red}]}
+            style={[
+              Style.AbsolutePosition.RightTop,
+              {
+                transform: [{rotate: '90deg'}],
+              },
+            ]}
+          />
+          <BorderComponent
+            width={20}
+            height={20}
+            borderStyle={[{backgroundColor: Color.red, borderColor: Color.red}]}
+            style={[
+              Style.AbsolutePosition.BottomRight,
+              {
+                transform: [{rotate: '180deg'}],
+              },
+            ]}
+          />
+        </View>
+      </TouchableOpacity>
+    );
+  },
+);
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,10 +1,12 @@
-import {AuthenticationStackParamList, HomeStackParamList} from '@navigation';
+import {HomeTicketLongFormComponent} from '@components';
+import {useBackButtonWithNavigation} from '@hooks';
+import {HomeStackParamList} from '@navigation';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {Label} from '@shared';
-import {Style} from '@styles';
-import React from 'react';
-import {View} from 'react-native';
+import {Icon, ScreenUtils, SimpleHeaderView} from '@shared';
+import {Color, Style} from '@styles';
+import React, {useCallback} from 'react';
+import {ScrollView, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 type NavigationProp = StackNavigationProp<HomeStackParamList, 'Home'>;
@@ -19,12 +21,132 @@ export const HomeScreen = React.memo((props?: HomeScreenProps) => {
   const route = useRoute<NavigationRoute>();
   const safeAreaInsets = useSafeAreaInsets();
 
+  useBackButtonWithNavigation(
+    React.useCallback(() => {
+      return true;
+    }, []),
+  );
+
+  const renderHeaderLeftView = useCallback(() => {
+    return (
+      <Icon.Button
+        name="ic_menu"
+        size={'large'}
+        color={Color.white}
+        style={[Style.Space.PaddingHorizontal.Zero]}
+      />
+    );
+  }, []);
+
+  const renderHeaderRightView = useCallback(() => {
+    return (
+      <View style={[Style.Size.FlexRow]}>
+        <Icon.Button
+          name="ic_cart"
+          size={'large'}
+          color={Color.white}
+          style={[Style.Space.PaddingHorizontal.Medium_12]}
+        />
+        <Icon.Button
+          name="ic_notification"
+          size={'large'}
+          color={Color.white}
+          style={[Style.Space.PaddingHorizontal.Zero]}
+        />
+      </View>
+    );
+  }, []);
+
+  const renderHeader = useCallback(() => {
+    return (
+      <SimpleHeaderView
+        disableRenderStatusBarView={true}
+        leftView={renderHeaderLeftView()}
+        rightView={renderHeaderRightView()}
+        style={[
+          Style.Background.Red,
+          Style.Space.PaddingHorizontal.large_16,
+          {marginTop: safeAreaInsets.top, justifyContent: 'space-between'},
+        ]}
+      />
+    );
+  }, []);
+
+  const renderBanner = useCallback(() => {
+    return (
+      <View
+        style={[
+          Style.Background.White,
+          Style.Size.WidthMatchParent,
+          Style.Space.MarginTop.large_16,
+          Style.Border.Standard.dialog,
+          Style.Space.PaddingHorizontal.Medium_12,
+          {height: ScreenUtils.getSizeByHorizontal(120)},
+        ]}
+      />
+    );
+  }, []);
+
+  const renderKenoTicket = useCallback(() => {
+    return (
+      <HomeTicketLongFormComponent
+        image="https://media.vietlott.vn//main/06.2018/cms/game/keno.png"
+        type="keno"
+      />
+    );
+  }, []);
+
+  const renderMegaTicket = useCallback(() => {
+    return (
+      <HomeTicketLongFormComponent
+        image="https://media.vietlott.vn//main/06.2018/cms/game/mega-645_full-color_cut-copy.png"
+        type="mega"
+      />
+    );
+  }, []);
+
+  const renderMaxTicket = useCallback(() => {
+    return (
+      <HomeTicketLongFormComponent
+        image="https://media.vietlott.vn//main/04.2019/bcc/game/thumbnail_max3d-01.jpg"
+        type="max"
+      />
+    );
+  }, []);
+
+  const renderPowerTicket = useCallback(() => {
+    return (
+      <HomeTicketLongFormComponent
+        image="https://media.vietlott.vn//main/06.2018/cms/game/Power655.png"
+        type="power"
+      />
+    );
+  }, []);
+
+  const renderMaxPlusTicket = useCallback(() => {
+    return (
+      <HomeTicketLongFormComponent
+        image="https://media.vietlott.vn//main/logo/logomax3dpro.png"
+        type="max-pro"
+      />
+    );
+  }, []);
+
   return (
-    <View style={{flex: 1}}>
-      <Label.Widget
-        style={[Style.Label.Bold.RedContentXL_16, Style.Label.Align.Center]}>
-        {'Home home'}
-      </Label.Widget>
-    </View>
+    <>
+      <View style={[Style.Size.MatchParent, Style.Background.Red]}>
+        {renderHeader()}
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={[Style.Space.MarginHorizontal.large_16]}>
+          {renderBanner()}
+          {renderKenoTicket()}
+          {renderPowerTicket()}
+          {renderMegaTicket()}
+          {renderMaxTicket()}
+          {renderMaxPlusTicket()}
+        </ScrollView>
+      </View>
+    </>
   );
 });
