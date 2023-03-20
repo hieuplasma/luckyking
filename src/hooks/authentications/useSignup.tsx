38ae07d12099c2f729c2748a70a86f8b5fe45bd5
HungPham <hungpg@vietpointer.vn>
2023-03-21 01:34:28 +0700

    update signup screen

---
+++ src/hooks/authentications/useSignup.tsx
@@ -0,0 +1,67 @@
+import {authApi} from '@api';
+import {useBase} from '@shared';
+import {useCallback, useMemo, useState} from 'react';
+import DeviceInfo from 'react-native-device-info';
+
+export const useSignup = () => {
+  const [password, setPassword] = useState<string | undefined>(undefined);
+  const [repeatPassword, setRepeatPassword] = useState<string | undefined>(
+    undefined,
+  );
+  const [phoneNumber, setPhoneNumber] = useState<string | undefined>(undefined);
+  const [errorMessage, setErrorMessage] = useState<
+    | {
+        phonenumber?: string;
+        password?: string;
+        repeatpassword?: string;
+      }
+    | undefined
+  >(undefined);
+  const {isLoading, setLoading} = useBase();
+
+  const deviceId = DeviceInfo.getDeviceId();
+
+  const onSignupPress = useCallback(() => {
+    setLoading(true);
+    return authApi
+      .register({
+        phoneNumber: phoneNumber,
+        password: password,
+        deviceId: deviceId,
+      })
+      .then(res => {
+        console.log('res', res);
+        return Promise.resolve(res);
+      })
+      .catch(err => {
+        console.log('err', err);
+        return Promise.reject(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [phoneNumber, password, deviceId]);
+
+  const onChangePhoneNumber = useCallback((phoneNumber?: string) => {
+    setPhoneNumber(phoneNumber);
+  }, []);
+
+  const onChangePassword = useCallback((password?: string) => {
+    setPassword(password);
+  }, []);
+  const onChangeRepeatPassword = useCallback((password?: string) => {
+    setRepeatPassword(password);
+  }, []);
+
+  return {
+    isLoading,
+    password,
+    phoneNumber,
+    errorMessage,
+    onSignupPress,
+    onChangePhoneNumber,
+    onChangePassword,
+    repeatPassword,
+    onChangeRepeatPassword,
+  };
+};
--- src/screens/authentications/SignUpScreen.tsx
+++ src/screens/authentications/SignUpScreen.tsx
@@ -1,7 +1,18 @@
-import {AuthenticationStackParamList} from '@navigation';
+import {useSignup} from '@hooks';
+import {AuthenticationStackParamList, ScreenName} from '@navigation';
 import {RouteProp, useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import React from 'react';
+import {
+  Icon,
+  InputComponent,
+  Label,
+  NavigationUtils,
+  ShadowView,
+  translate,
+} from '@shared';
+import {Color, Style} from '@styles';
+import {Button} from '@widgets';
+import React, {useCallback} from 'react';
 import {View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
@@ -19,6 +30,119 @@ export interface SignUpScreenProps {}
 export const SignUpScreen = React.memo((props?: SignUpScreenProps) => {
   const navigation = useNavigation<NavigationProp>();
   const safeAreaInsets = useSafeAreaInsets();
+  const signupHooks = useSignup();
 
-  return <View style={{flex: 1, backgroundColor: 'pink'}}></View>;
+  const onSignupClick = useCallback(() => {
+    // signupHooks
+    //   .onSignupPress()
+    //   .then(() => {
+    //     NavigationUtils.navigate(navigation, ScreenName.Main);
+    //   })
+    //   .catch();
+    NavigationUtils.navigate(navigation, ScreenName.Authentications.VerifyOTP);
+  }, [signupHooks.onSignupPress, navigation]);
+
+  const onGoBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  const renderNumberInput = useCallback(() => {
+    return (
+      <InputComponent
+        editable={true}
+        keyboardType="phone-pad"
+        value={signupHooks.phoneNumber}
+        placeholder={translate('input.phoneNumber')}
+        label={translate('input.phoneNumber')}
+        onChangeText={signupHooks.onChangePhoneNumber}
+        containerStyle={[Style.Space.MarginTop.xLarge_24]}
+        errorMessage={signupHooks.errorMessage?.phonenumber}
+      />
+    );
+  }, [signupHooks.phoneNumber, signupHooks.errorMessage?.phonenumber]);
+
+  const renderPasswordInput = useCallback(() => {
+    return (
+      <InputComponent
+        editable={true}
+        keyboardType="default"
+        value={signupHooks.password}
+        placeholder={translate('input.password')}
+        label={translate('input.password')}
+        onChangeText={signupHooks.onChangePassword}
+        containerStyle={[Style.Space.MarginTop.xLarge_24]}
+        errorMessage={signupHooks.errorMessage?.password}
+        secureTextEntry={true}
+      />
+    );
+  }, [signupHooks.password, signupHooks.errorMessage?.password]);
+
+  const renderRepeatPasswordInput = useCallback(() => {
+    return (
+      <InputComponent
+        editable={true}
+        keyboardType="default"
+        value={signupHooks.repeatPassword}
+        placeholder={translate('input.repeatPassword')}
+        label={translate('input.repeatPassword')}
+        onChangeText={signupHooks.onChangeRepeatPassword}
+        containerStyle={[Style.Space.MarginTop.xLarge_24]}
+        errorMessage={signupHooks.errorMessage?.password}
+        secureTextEntry={true}
+      />
+    );
+  }, [signupHooks.repeatPassword, signupHooks.errorMessage?.password]);
+
+  const renderSignupButton = useCallback(() => {
+    return (
+      <Button.Widget
+        text={'button.signUp'}
+        type="primary"
+        style={[
+          Style.Background.Primary,
+          Style.Self.Center,
+          Style.Space.MarginTop.large_16,
+        ]}
+        onClicked={onSignupClick}
+        isLoading={signupHooks.isLoading}
+      />
+    );
+  }, [onSignupClick, signupHooks.isLoading]);
+
+  return (
+    <View
+      style={[
+        Style.Size.MatchParent,
+        Style.Background.Red,
+        Style.Space.PaddingHorizontal.large_16,
+        Style.Content.CenterInVertical,
+      ]}>
+      <ShadowView>
+        <View style={[Style.Size.FlexRow]}>
+          <Icon.Button
+            size={'small'}
+            color={Color.gray}
+            name="ic_back"
+            style={[Style.Space.Padding.Zero]}
+            onPressed={onGoBack}
+          />
+          <Label.Widget
+            uppercase
+            style={[
+              Style.Size.MatchParent,
+              Style.Label.Regular.WhiteContentXL_16,
+              Style.Label.Align.Center,
+              Style.Space.MarginRight.largeMargin_16,
+              {color: Color.black},
+            ]}>
+            {translate('label.signup')}
+          </Label.Widget>
+        </View>
+        {renderNumberInput()}
+        {renderPasswordInput()}
+        {renderRepeatPasswordInput()}
+        {renderSignupButton()}
+      </ShadowView>
+    </View>
+  );
 });
