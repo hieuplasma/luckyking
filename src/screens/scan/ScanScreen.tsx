716deb96cf31103639ead751d2669db8911ff608
Hieu Tran <phanthang@Hieus-MacBook-Pro-2.local>
2023-05-05 10:13:35 +0700

    init feature scan barcode lottery

--- src/screens/scan/ScanScreen.tsx
+++ src/screens/scan/ScanScreen.tsx
@@ -1,17 +1,58 @@
-import {ScanStackParamList} from '@navigation';
-import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import { ScanStackParamList } from '@navigation';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type NavigationProp = StackNavigationProp<ScanStackParamList, 'Scan'>;
 type NavigationRoute = RouteProp<ScanStackParamList, 'Scan'>;
 
-export interface ScanScreenParamsList {}
+export interface ScanScreenParamsList { }
 
-export const ScanScreen = () => {
-  const navigation = useNavigation<NavigationProp>();
-  const route = useRoute<NavigationRoute>();
-  const safeAreaInsets = useSafeAreaInsets();
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { scanBarCode } from './barcode';
 
-  return <></>;
-};
+export const ScanScreen = React.memo(() => {
+  const [hasPermission, setHasPermission] = useState<any>(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      console.log(status, scanned)
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }: any) => {
+    setScanned(true);
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    console.log(data)
+    alert(`Data scan được\n${scanBarCode(data)}`)
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        // style={StyleSheet.absoluteFillObject}
+        style={{ flex: 1 }}
+      />
+      {scanned && <Button title={'Nhấn để scan lại'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+})
+
+const styles = StyleSheet.create({
+  container: { flex: 1 }
+}); 
