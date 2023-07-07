9fcd9add9cde2391caa1df766263ac8ab5bfca1d
Hieu Tran <phanthang@Hieus-MacBook-Pro-2.local>
2023-07-07 15:03:35 +0700

    remove expo for android/

--- android/app/src/main/java/com/luckyking_app_react/MainApplication.java
+++ android/app/src/main/java/com/luckyking_app_react/MainApplication.java
@@ -1,7 +1,4 @@
 package com.luckyking_app_react;
-import android.content.res.Configuration;
-import expo.modules.ApplicationLifecycleDispatcher;
-import expo.modules.ReactNativeHostWrapper;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
@@ -17,7 +14,7 @@ import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {
+       new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -47,7 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected Boolean isHermesEnabled() {
           return BuildConfig.IS_HERMES_ENABLED;
         }
-      });
+      };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -63,12 +60,5 @@ public class MainApplication extends Application implements ReactApplication {
       DefaultNewArchitectureEntryPoint.load();
     }
     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    ApplicationLifecycleDispatcher.onApplicationCreate(this);
-  }
-
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
   }
 }
